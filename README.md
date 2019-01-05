4a9ff0a2083e5ca5da18376f4b811f28ca76224e
Hyeseong Kim <cometkim.kr@gmail.com>
2019-01-05 19:49:03 +0900

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # react-hot-reload.macro
-Zero configuration hot reloading using Babel macro
+
+[![Babel Macro](https://img.shields.io/badge/babel--macro-%F0%9F%8E%A3-f5da55.svg?style=flat-square)](https://github.com/kentcdodds/babel-plugin-macros)
+
+Zero configuration :fire: hot reloading :fire: using [Babel macro](https://github.com/kentcdodds/babel-plugin-macros)
+
+Using [react-hot-loader](https://github.com/gaearon/react-hot-loader), mostly for Create React App (v2+) users
+
+## Usage
+
+```bash
+yarn add react-hot-reload.macro
+
+or
+
+npm install --save react-hot-reload.macro
+```
+
+Modify your `App.js` component
+
+```diff
+import React from 'react'
++import hot from 'react-hot-reload.macro'
+
+...
+
+-export default App
++export default hot(module)(App)
+```
+
+That's it. Happy Hacking!
---
+++ index.js
@@ -0,0 +1,47 @@
+const { createMacro, MacroError } = require('babel-plugin-macros')
+const plugin = require('react-hot-loader/babel')
+
+function macro({ babel, references, state }) {
+    if (!references.default) {
+        throw new MacroError('react-hot-reload.macro requires a default import')
+    }
+
+    const { types: t } = babel
+    const { node: referenceNode, scope } = references.default[0]
+
+    if (process.env.NODE_ENV === 'production') {
+        state.file.path.traverse({
+            CallExpression({ node, parentPath }) {
+                if (node.callee.name !== referenceNode.name) {
+                    return
+                }
+
+                parentPath.replaceWith(t.identifier(
+                    parentPath.node.arguments[0].name,
+                ))
+            },
+        })
+        scope.removeBinding(referenceNode.name)
+    } else {
+        // Do apply react-hot-loader plugin first
+        plugin(babel)
+
+        state.file.path.traverse({
+            ImportDefaultSpecifier({ node, parentPath }) {
+                if (node.local.name !== referenceNode.name) {
+                    return
+                }
+
+                const identifier = t.identifier(referenceNode.name)
+                const declaration = t.importDeclaration(
+                    [t.importSpecifier(identifier, identifier)],
+                    t.stringLiteral('react-hot-loader'),
+                )
+
+                parentPath.insertAfter(declaration)
+            },
+        })
+    }
+}
+
+module.exports = createMacro(macro)
